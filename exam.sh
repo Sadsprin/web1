6bb9810bbcdf2e1f7659f8370f8769d1762bc0a8
Sadsprin <54733868+Sadsprin@users.noreply.github.com>
2021-07-27 08:57:15 -0400

    Add files via upload

--- exam.sh
+++ exam.sh
@@ -8,32 +8,34 @@ do
 	case "${flag}" in
 		b) BENCHMARKS=${OPTARG};;
 		d) DISABLE_WAL=${OPTARG};;
-		f) FILEPATH=${OPTARG};;
+		f) FILEPATH=${OPTARG};; # FILEPATH의 이름은 아무거나 해도됨 파일이 없다면 자동으로 만들어짐
 	esac
 done
 
-read -r -p "please enter value size separated by space: " -a arr
+read -r -p "please enter value size separated by space: " -a arr # space마다 value_size가 arr에 들어감 ex) 16 100 200 arr = (16,100,200)
 
-cat /dev/null > $FILEPATH
+cat /dev/null > $FILEPATH # FILEPATH에 있는 내용을 삭제함
 
-for value_size in "${arr[@]}"
-
-do
-
-cat /dev/null > final_result.txt
-
-for number in {1..5}
+for value_size in "${arr[@]}" # arr에 있는 value_size를 하나씩 꺼냄
 
 do
 
-./db_bench  --benchmarks="$BENCHMARKS" --disable_wal="$DISABLE_WAL" --value_size="$value_size" > my_result.txt
+cat /dev/null > final_result.txt # 밑의 반복문이 반복될때 마다  final_result.txt를 지움 test_pasing에서는 
 
-sed -n '22p' my_result.txt >> final_result.txt
+for number in {1..5} # 5번의 task case
+
+do
+
+./db_bench  --benchmarks="$BENCHMARKS" --disable_wal="$DISABLE_WAL" --value_size="$value_size" > my_result.txt # db_bench 실행
+
+sed -n '22p' my_result.txt >> final_result.txt # my_result.txt의 22번째 줄의 내용을 final_result.txt에 저장
 
 done
 
-python3 test_pasing.py $FILEPATH
+python3 test_pasing.py $FILEPATH # final_result.txt의 내용 중 성능 부분만 parsing하여 csv파일을 만듦
 
 done
 
 exit 0
+
+# 끝
--- test_pasing.py
+++ test_pasing.py
@@ -1,52 +1,52 @@
 import re
 import csv
-import numpy as np
 import os
 import sys
 
-if len(sys.argv) > 1:
-    file_path = sys.argv[1]
+if len(sys.argv) > 1: #FILEPATH 안넣으면 에러처리
+    file_path = sys.argv[1] # 첫 인자 가져옴
 else:
     print("please input csv file_path.")
     sys.exit(1)
 
 def write_to_file_from_list(filepath, to_csv_list):
-    if os.stat(filepath).st_size == 0:
-        with open(filepath, 'w', encoding='utf-8') as f:
-            writer = csv.writer(f)
-            writer.writerow(fields)
-            writer.writerow(to_csv_list)
+    if os.path.isfile(filepath) == False: # 파일이 존재하지 않을때
+        mod = 'w' # write mode
     else:
-        with open(filepath,'a',encoding='utf-8') as f:
+        mod = 'a' # append mode
+
+    with open(filepath, mod, encoding='utf-8') as f: #파일을 열고, file이 없으면 새로 만듬
             writer = csv.writer(f)
-            writer.writerow(to_csv_list)
+            if os.stat(filepath).st_size == 0: # 파일에 아무 내용이 없을 때 
+                writer.writerow(fields) # csv_file columns
+            writer.writerow(to_csv_list) # db_bench 실행 결과 파싱한 list를  csv파일에 씀
     f.close()
 
-r = open("./final_result.txt",mode='r',encoding='utf-8')
-fields = ['latency', 'ops/sec', 'throughput(MB/s)']
-regex = re.compile(r'\d+\.?\d+')
+# initialize variable
+latency = 0 
+ops = 0
+throughput = 0
+
+r = open("./final_result.txt",mode='r',encoding='utf-8') # final_result.txt를 가져와서 
+fields = ['latency', 'ops/sec', 'throughput(MB/s)'] # csv파일의 columns list
+regex = re.compile(r'\d+\.?\d+') # regular expression ./final_result.txt의 내용중에 숫자만 추출하는 코드
 for i in range(5):
-    oneline_result = r.readline()
-    to_row = regex.findall(oneline_result)
-    latency_, ops_, throughput_ = map(float, to_row)
+    oneline_result = r.readline() # final_result.txt를 한줄 읽어서 oneline_result에 저장 다음 반복에서는 다음줄로
+    to_row = regex.findall(oneline_result) # 위 regex를 이용하여 추출한 숫자를 리스트로 만듦
+    latency_, ops_, throughput_ = map(float, to_row) # 리스트를 분해해서 3개의 변수로 나눔
 
-    if i == 0:
-        latency = latency_
-        ops = ops_
-        throughput = throughput_
-    if i != 0:
-        latency += latency_
-        ops += ops_
-        throughput += throughput_
+# db_bench  result add!
+    latency += latency_
+    ops += ops_
+    throughput += throughput_
 
-latency = latency / 5
-ops = ops / 5
-throughput = throughput / 5
+latency = round(latency / 5, 2)# 5개의 latency를 평균낸 값
+ops = round(ops / 5, 2)# 5개의 ops를 평균낸 값
+throughput = round(throughput / 5, 2)# 5개의 throughput을 평균낸 값
 
-list_for_csv = [latency, ops, throughput]
+list_for_csv = [latency, ops, throughput] # 리스트로 만들어서 저장
 
-print(list_for_csv)
-write_to_file_from_list(file_path, list_for_csv)
+write_to_file_from_list(file_path, list_for_csv) # write_to_file_from_list를 이용하여 위 리스트의 값을 file_path(csv file)에 씀
 
 
 
